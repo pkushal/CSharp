7cdee9550fc56cd57a4eee527d42651d7c709844
Kushal Pradhan <pkushal@uab.edu>
2015-01-15 18:47:56 -0600

    Dictionary

---
+++ CodeSnippet/CodeSnippet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSnippet
+{
+    class Program
+    {
+
+        /**
+         Keyboard shortcut: Ctrl K + X
+         Readymade snippets of code you can quickly insert into your code.
+
+        Highlight the code and Ctrl + K + S
+         */
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                
+            }
+        }
+    }
+}
